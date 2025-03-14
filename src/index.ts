8eb8c985b392d5a50629bbfebcb11854ba268dae
Alien <minhhnghiii@gmail.com>
2025-03-14 23:15:37 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("App working");
+});
